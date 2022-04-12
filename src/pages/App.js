a37985c584d7a74f9a9537129a14a76a003b1ccc
Juan Oriana <joriana@itba.edu.ar>
2022-04-12 16:07:11 -0300

    Intelligent optimal task finding

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Navbar, Footer, TaskListUnit } from "../components";
 import { GoPlus } from "react-icons/go";
@@ -6,12 +6,13 @@ import { toggleTypes, turnOnModal } from "../slices/taskModalSlice";
 import NewTaskListModal from "../components/Modal/NewTaskListModal";
 import NewTaskModal from "../components/Modal/NewTaskModal";
 import EditTaskModal from "../components/Modal/EditTaskModal";
+import { findOptimalTask } from "../utils/finders";
 
 function App() {
   const taskLists = useSelector((state) => state.taskLists) || [];
   const taskModal = useSelector((state) => state.taskModal);
   const dispatch = useDispatch();
-
+  const optimalTask = useMemo(() => findOptimalTask(taskLists), [taskLists]);
   const renderProperModal = (toggleType) => {
     switch (toggleType) {
       case toggleTypes.TOGGLE_NEW_LIST:
@@ -29,21 +30,44 @@ function App() {
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex flex-col flex-1">
-          <button
-            className="bg-brand-dark w-64 self-center mt-8 mb-3 text-white font-bold
-            rounded-2xl hover:bg-brand-light
-             px-3 py-1 ease-in-out duration-150"
-            onClick={() =>
-              dispatch(turnOnModal({ type: toggleTypes.TOGGLE_NEW_LIST }))
-            }
-          >
-            <div className="flex items-center justify-around">
-              Add a new task group
-              <div className="text-4xl rounded-full ml-2">
-                <GoPlus />
+          <div className="flex w-full justify-evenly w-9/12 self-center items-center ">
+            {taskLists.length > 0 && optimalTask && (
+              <div className="flex">
+                <p
+                  className="bg-brand-main w-80 self-center mb-3 text-white p-4
+            rounded-2xl rounded-br-sm text-center -mt-8"
+                >
+                  You should probably be working on that{" "}
+                  <b>{optimalTask.name} </b>
+                  activity in your{" "}
+                  <b className="text-orange-400">{optimalTask.listName}</b>{" "}
+                  group!
+                </p>
+                <img
+                  src="/images/lil_guy.svg"
+                  className="w-64 mt-12"
+                  alt="guy"
+                />
               </div>
-            </div>
-          </button>
+            )}
+            <button
+              className={`flex bg-brand-dark w-64 self-center ${
+                taskLists > 0 ? "mt-8 mb-3" : "mt-14 -mb-4"
+              } mb-3 text-white font-bold
+            rounded-2xl hover:bg-brand-light items-center justify-center
+             px-3 py-1 ease-in-out duration-150`}
+              onClick={() =>
+                dispatch(turnOnModal({ type: toggleTypes.TOGGLE_NEW_LIST }))
+              }
+            >
+              <div className="flex flex-col items-center justify-center text-xl w-21">
+                Add a new task group
+                <div className="text-4xl rounded-full">
+                  <GoPlus />
+                </div>
+              </div>
+            </button>
+          </div>
           <div className="flex p-4 flex-wrap">
             {taskLists.map((list) => (
               <TaskListUnit
